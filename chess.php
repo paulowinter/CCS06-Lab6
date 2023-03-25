058d0fc4b9aa08d53e21272d9a5a0f37f9f427f1
paulo bondoc <paulo.pikatchu@gmail.com>
2023-03-25 18:55:32 +0800

    Files lab

---
+++ chess.php
@@ -0,0 +1,78 @@
+<body>
+
+<div class="chessboard">
+<!-- 1st -->
+<div class="white"></div>
+<div class="black"></div>
+<div class="white"></div>
+<div class="black"><?php echo $pieces['black']['queen']; ?></div>
+<div class="white"><?php echo $pieces['black']['king']; ?></div>
+<div class="black"></div>
+<div class="white"></div>
+<div class="black"></div>
+<!-- 2nd -->
+<div class="black"></div>
+<div class="white"></div>
+<div class="black"></div>
+<div class="white"></div>
+<div class="black"></div>
+<div class="white"></div>
+<div class="black"></div>
+<div class="white"></div>
+<!-- 3th -->
+<div class="white"></div>
+<div class="black"></div>
+<div class="white"></div>
+<div class="black"></div>
+<div class="white"></div>
+<div class="black"></div>
+<div class="white"></div>
+<div class="black"></div>
+<!-- 4st -->
+<div class="black"></div>
+<div class="white"></div>
+<div class="black"></div>
+<div class="white"></div>
+<div class="black"></div>
+<div class="white"></div>
+<div class="black"></div>
+<div class="white"></div>
+<!-- 5th -->
+<div class="white"></div>
+<div class="black"></div>
+<div class="white"></div>
+<div class="black"></div>
+<div class="white"></div>
+<div class="black"></div>
+<div class="white"></div>
+<div class="black"></div>
+<!-- 6th -->
+<div class="black"></div>
+<div class="white"></div>
+<div class="black"></div>
+<div class="white"></div>
+<div class="black"></div>
+<div class="white"></div>
+<div class="black"></div>
+<div class="white"></div>
+<!-- 7th -->
+<div class="white"></div>
+<div class="black"></div>
+<div class="white"></div>
+<div class="black"></div>
+<div class="white"></div>
+<div class="black"></div>
+<div class="white"></div>
+<div class="black"></div>
+<!-- 8th -->
+<div class="black"></div>
+<div class="white"><?php echo $pieces['white']['knight']; ?></div>
+<div class="black"></div>
+<div class="white"><?php echo $pieces['white']['king']; ?></div>
+<div class="black"></div>
+<div class="white"></div>
+<div class="black"><?php echo $pieces['white']['knight']; ?></div>
+<div class="white"></div>
+</div>
+</body>
+</html>
